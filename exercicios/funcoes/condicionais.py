3501ab4871250622818194cc9e43d419cfc560fd
Alexandre-up <repository357@gmail.com>
2026-09-25 00:45:23 -0300

    Funções - Condicionais - Loops

--- exercicios/funcoes/condicionais.py
+++ exercicios/funcoes/condicionais.py
@@ -1,4 +1,4 @@
-#Exercicio 1
+#Exercício 1
 def fizz_buzz(numero:int):
     if numero % 3 == 0 and numero % 5 == 0:
             return "fizzbuzz"
@@ -9,21 +9,21 @@ def fizz_buzz(numero:int):
     else:
          return numero
     
-#Exercicio 2
+#Exercício 2
 def verificar_maioridade(idade:int):
       if idade < 18:
         return "Menor de idade"
       else:
            return "Maior de idade"
       
-#Exercicio 3
+#Exercício 3
 def verificar_paridade(numero:int) -> bool:
       if numero % 2 == 0:
         return "É par"
       else:
            return "Não é par"
       
-#Exercicio 4
+#Exercício 4
 def classificar_numero(numero:int) -> bool:
       if numero == 0:
         return "Zero"
@@ -32,7 +32,7 @@ def classificar_numero(numero:int) -> bool:
       else:
            return "Número positivo"
       
-#Exercicio 5
+#Exercício 5
 def calcular_resultado(nota1:float, nota2:float):
       media = (nota1+nota2) / 2
       if media >= 7.0:
@@ -40,7 +40,7 @@ def calcular_resultado(nota1:float, nota2:float):
       else:
         return "Reprovado"
 
-#Exercicio 6
+#Exercício 6
 def maior_de_dois(a:int, b:int) -> float:
       if a < b:
         return "O segundo é maior"
@@ -49,7 +49,7 @@ def maior_de_dois(a:int, b:int) -> float:
       else:
         return "São iguais"
 
-#Exercicio 7
+#Exercício 7
 def calcular_desconto(valor_compra:float, cliente_vip:str) -> bool:
     if (cliente_vip == True) or (valor_compra > 200):
       desconto = 0.15
@@ -58,7 +58,7 @@ def calcular_desconto(valor_compra:float, cliente_vip:str) -> bool:
     valor_final = valor_compra * (1 - desconto)
     return f"Valor final: R$ {valor_final:.2f}"
 
-#Exercicio 8
+#Exercício 8
 def conceito_nota(nota:int) -> float:
       if nota >= 9.0:
         return "A"
@@ -69,7 +69,7 @@ def conceito_nota(nota:int) -> float:
       else:
         return "F"
       
-#Exercicio 9
+#Exercício 9
 def tipo_triangulo(a:float, b:float, c:float):
     if (a+b > c) and (b+c >a) and (a+c >b):
         if a != b != c:
@@ -81,7 +81,7 @@ def tipo_triangulo(a:float, b:float, c:float):
         else:
             return "Não é Triângulo"     
         
-#Exercicio 10
+#Exercício 10
 def calcular_imposto(salario:float):
     if salario <= 2000.00:
         return 0.0
@@ -89,7 +89,7 @@ def calcular_imposto(salario:float):
       return (salario - 2000.00) * 0.10
     else:
         return 200.00 + (salario - 4000.00) * 0.20
-    
+
 #Exercício 11
 def validador_bissexto(ano:int):
     if ano % 400 == 0:
@@ -106,29 +106,29 @@ if __name__ == '__main__':
 
     print("\n=> EXERCICIOS <======================================\n")
     teste = fizz_buzz(27)
-    print(f"1º → {teste}")
+    print(f"1 → {teste}")
     idade = verificar_maioridade(17)
-    print(f"2º → {idade}")
+    print(f"2 → {idade}")
     paridade = verificar_paridade(23)
-    print(f"3º → {paridade}")
+    print(f"3 → {paridade}")
     numero = classificar_numero(0)
-    print(f"4º → {numero}")
+    print(f"4 → {numero}")
     media = calcular_resultado(8.5,6.5)
-    print(f"5º → {media}")
+    print(f"5 → {media}")
     numero = maior_de_dois(8.5,8.5)
-    print(f"6º → {numero}")
+    print(f"6 → {numero}")
     desconto = calcular_desconto(100.0, False)
-    print(f"7º → {desconto}")
+    print(f"7 → {desconto}")
     conceito = conceito_nota(8.9)
-    print(f'8º → Conceito "{conceito}"')
+    print(f'8 → Conceito "{conceito}"')
     triangulo = tipo_triangulo(6,7,9)
-    print(f"9º → {triangulo}")
+    print(f"9 → {triangulo}")
     imposto1 = calcular_imposto(1800.00)
     imposto2 = calcular_imposto(3000.00)
     imposto3 = calcular_imposto(5000.00)
-    print(f"10º → 1800 = {imposto1} | 3000 = {imposto2} | 5000 = {imposto3}")
+    print(f"10 → 1800 = {imposto1} | 3000 = {imposto2} | 5000 = {imposto3}")
     bissexto1 = validador_bissexto(2024)
     bissexto2 = validador_bissexto(1900)
     bissexto3 = validador_bissexto(2000)
-    print(f"11º → Ano 2024 = {bissexto1} | Ano 1900 = {bissexto2} | Ano 2000 = {bissexto3}")
+    print(f"11 → Ano 2024 = {bissexto1} | Ano 1900 = {bissexto2} | Ano 2000 = {bissexto3}")
     print("\n=====================================================")
--- exercicios/funcoes/loop.py
+++ exercicios/funcoes/loop.py
@@ -99,36 +99,34 @@ def simular_saque(saldo_inicial:float, saques:list):
     return saldo
 
 #Exercício 11
-def remover_duplicados(lista:list):
+def remover_duplicados(lista):
     resultado = []
-    for elemento in lista:
-        if elemento in lista:
-            if elemento not in resultado:
-                resultado.append(elemento)
-    
-    return resultado
+    for elemento in  lista:
+        if elemento not in resultado:
+            resultado.append(elemento)
 
+    return resultado
+    
 #Exercício 12
 def media_positivos(numeros:list):
     soma = 0
     quantidade = 0
     for numero in numeros:
-        if numero in numeros:
-            if numero > 0:
-                soma += numero
-                quantidade += 1
+        if numero > 0:
+            soma += numero
+            quantidade += 1            
     if quantidade == 0:
-        return 0.0
+        return 0.0        
     return soma / quantidade
-
+    
 #Exercício 13
 def validar_senhas(lista_senhas:list[str]):
-    senhas_validas =[]
+    senhas_validas = []
     for senha in lista_senhas:
-        if len(senha) >=8:
+        if len(senha) >= 8:
             senhas_validas.append(senha)
     return senhas_validas
-
+    
 #Exercício 14
 def primeiro_impar(numeros):
     i = 0
@@ -137,32 +135,86 @@ def primeiro_impar(numeros):
             return numeros[i]
         i += 1
     return None
-
+    
 #Exercício 15
 def contar_ocorrencias(lista:list, alvo):
     quant = 0
     for obj in lista:
         if obj == alvo:
-                quant += 1
+            quant += 1
     return quant
-
-#Exercício 16
-def estrit_crescente(palavras:list):
+    
+#ExercÍcio 16
+def estritamente_crescente(palavras:list):
     i = 1
-    while i < len(palavras) - 1:
-        if palavras[i] >= palavras[i-1]:
-            return False    
-        i += 1
+    
+    while i < len(palavras):
+        if len(palavras[i]) <= len(palavras[i-1]):
+            return False            
+        i += 1        
     return True
 
-
+#Exercício 17
+def mover_zeros_final(numeros:list):
+    zeros = []
+    
+    for numero in numeros:
+        if numero != 0:
+            zeros.append(numero)
+            
+    for numero in numeros:
+        if numero ==0:
+            zeros.append(numero)
+    
+    return zeros
+    
+#Exercício 18
+def processar_fila(clientes:list[tuple]):
+    preferenciais = []
+    demais = []
+    
+    for nome, idade in clientes:
+        if idade >= 60:
+            preferenciais.append(nome)
+        else:
+            demais.append(nome)
+            
+    return preferenciais + demais
+    
+#Exercício 19
+def encontrar_picos(numeros:list):
+    picos = []
+    i = 1
+    
+    while i < len(numeros) - 1:
+        if numeros[i] > numeros[i-1] and numeros[i] > numeros [i+1]:
+            picos.append(numeros[i])
+            
+        i += 1
+        
+    return picos
+    
+#Exercício 20
+def validar_extrato(saldo_inicial:float, transacoes:list):
+    saldo = saldo_inicial
+    i = 0
+    
+    while i < len(transacoes):
+        saldo += transacoes[i]
+        
+        if saldo < 0:
+            return f"Extato Inválido: Saldo Negativo na Posição {i}"
+            
+        i += 1
+        
+    return f"Extrato Válido: Saldo Final {saldo:.2f}"
 
 
 if __name__ == '__main__':
 
     print("\n=> EXERCICIOS <======================================\n")
     print("-> → ", end="")
-    dobro = dobrar([1,2,3,4,5])   
+    dobro = dobrar([1,2,3,4,5])
     numeros_pares = filtrar_pares([1,2,3,4,5,6])
     print(f"\n1º → {numeros_pares}")
     n_negativos = contar_negativos([-3,-2,-1,0,1,2,3,])
@@ -172,7 +224,7 @@ if __name__ == '__main__':
     zerar = zerar_negativos([-3,-2,-1,0,1,2,3])
     print(f"4º → {zerar}")
     valor = contem_valor(["maçã", "banana", "uva"], "banana")
-    print(f"5º → {valor}")
+    print(f"5º → [{valor}]")
     aprovados = contar_aprovados([8.5,5.0,7.0,6.5,9.0])
     print(f"6º → [{aprovados}]")
     palavras = filtrar_palavras_curtas(["sol", "computador", "python", "mar"],6)
@@ -182,18 +234,26 @@ if __name__ == '__main__':
     extremos = encontrar_extremos([14,2,35,-4,20])
     print(f"9º → {extremos}")
     saque = simular_saque(200,[50,100,80,30])
-    print(f"10º → Saldo: R${saque},00")
+    print(f"10º → Saldo: [R${saque},00]")
     duplicados = remover_duplicados([1, 3, 2, 3, 1, 4, 2])
     print(f"11º → Duplicados: {duplicados}")
     media = media_positivos([-5, 10, -2, 20, 30])
     print(f"12º → {media}")
-    valida = validar_senhas(["12345", "senha1234", "admin", "python2026"])
-    print(f"13º → {valida}")
-    impar = primeiro_impar([2,4,6,7,9,10,13])
-    print(f"14º → [{impar}]")
+    validas = validar_senhas(["12345", "senha1234", "admin", "python2026"])
+    print(f"13º → {validas}")
+    primeiro = primeiro_impar([2,4,6,7,9,10,11,13])
+    print(f"14º → [{primeiro}]")
     contar = contar_ocorrencias(["a","b","a","c","a"], "a")
     print(f"15º → [{contar}]")
-    crescente1 = estrit_crescente(["lá","lua","porta","cinema","computador"])
-    crescente2 = estrit_crescente(["palito","geringonça","lápis","lâmpada"])
-    print(f"16º → {crescente1} | {crescente2}")
+    estrito1 = estritamente_crescente(["lá", "lua", "cinema", "computador"])
+    estrito2 = estritamente_crescente(["palito", "geringonça", "lápis", "lâmpada"])
+    print(f"16º → {estrito1} | {estrito2}")
+    mover = mover_zeros_final([0,1,0,3,12,0,5])
+    print(f"17º → {mover}")
+    fila = processar_fila([("Ana",25), ("Bento",67), ("Carla",18), ("Daniel",72)])
+    print(f"18º → {fila}")
+    resultado = encontrar_picos([1,5,2,6,3,1,8,4])
+    print(f"19º → {resultado}\n")
+    print(f"20º → Saldo1: {validar_extrato(100,[-50,-60,20])}")    
+    print(f"    → Saldo2: {validar_extrato(50,[30,-40,-20,100])}")
     print("\n=====================================================")
